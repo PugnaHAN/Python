4eb83b9f25ef18ffafa876ab5dd4f838816ee3ad
Pugna_HAN <justin_victory@hotmail.com>
2016-01-03 14:54:56 +0800

    python print_unicode.py world hello ...

--- Chapter_2/printUnicodeTable.py
+++ Chapter_2/printUnicodeTable.py
@@ -5,21 +5,22 @@ def print_unicode_table(word):
     print("decimal    hex    char    {0:^40}".format("name"))
     print("-------    ---    ----    {0:-<40}".format(""))
 
-    if word is None or word in name.lower():
-        code = ord(" ")
-        end = sys.maxunicode
-        while code < end:
-            c = chr(code)
-            name = unicodedata.name(c, "***unknown***")
-            print("{0:7} {0:6X}    {0:^5c}   {1}".format(code,
-                                                         name.title()))
-            code += 1
-    else:
-        for char in word:
-            code = ord(char)
-            name = unicodedata.name(char, "***unknown***")
-            print("{0:7} {0:6X}    {0:^5c}   {1}".format(ord(char),
-                                                         name.title()))
+    for i in range(len(word)):
+        if word[i] is None:
+            code = ord(" ")
+            end = sys.maxunicode
+            while code < end:
+                c = chr(code)
+                name = unicodedata.name(c, "***unknown***")
+                print("{0:7} {0:6X}    {0:^5c}   {1}".format(code,
+                                                             name.title()))
+                code += 1
+        else:
+            for char in word[i]:
+                code = ord(char)
+                name = unicodedata.name(char, "***unknown***")
+                print("{0:7} {0:6X}    {0:^5c}   {1}".format(ord(char),
+                                                             name.title()))
 
 word = None
 if len(sys.argv) > 1:
@@ -27,7 +28,10 @@ if len(sys.argv) > 1:
         print("usage: {0}[string]".format(sys.argv[0]))
         word = 0
     else:
-        word = sys.argv[1].lower()
+        length = len(sys.argv)
+        word = []
+        for i in range(length - 1):
+            word.append(sys.argv[1 + i])
 
 if word != 0:
   print_unicode_table(word)
