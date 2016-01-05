b6828b993890b3010e0320b56bdbb129cae7c874
Pugna_HAN <justin_victory@hotmail.com>
2016-01-05 16:13:23 +0800

    add some essentional file update on practice

--- Chapter_3/uniqueword.py
+++ Chapter_3/uniqueword.py
@@ -1,16 +1,18 @@
 import string
 import sys
+import collections
 
-words = {}
+words = collections.defaultdict(int)
 strip = string.whitespace + string.punctuation + string.digits + "\""
 
 for filename in sys.argv[1:]:
+    # print(open(filename))
     for line in open(filename):
         for word in line.lower().split():
             word = word.strip(strip)
             if len(word) > 2:
-                words[word] = words.get(word, 0) + 1
-                print(words)
-
-for word in sorted(words):
+                words[word] += 1
+                # print(words)
+# print(words.values())
+for word in sorted(words, key = lambda word : words[word]):
     print("'{0}' occurs {1} times".format(word, words[word]))
