d11a4dce38c97302a518c30228c7718f596407d0
Pugna_HAN <justin_victory@hotmail.com>
2016-01-19 21:59:53 +0800

    The answer of xdump

--- Chapter_7/xdump.py
+++ Chapter_7/xdump.py
@@ -1,64 +1,83 @@
+#!/usr/bin/env python3
+# Copyright (c) 2008-11 Qtrac Ltd. All rights reserved.
+# This program or module is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version. It is provided for educational
+# purposes and is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+
 import optparse
-import sys
 import os
-import struct
-from BinaryRecordFile import BinaryRecordFile
+import sys
+
 
 def main():
-    usage = "Usage: xdump.py [options] file1 [file2 [... fileN]"
-    parser = optparse.OptionParser(usage = usage)
+    parser = optparse.OptionParser(
+                usage="usage: %prog [options] file1 [file2 [... fileN]]")
+    parser.add_option("-b", "--blocksize", dest="blocksize", type="int",
+            help="block size (8..80) [default: %default]")
+    parser.add_option("-d", "--decimal", dest="decimal",
+            action="store_true",
+            help="decimal block numbers [default: hexadecimal]")
+    parser.add_option("-e", "--encoding", dest="encoding",
+            help="encoding (ASCII..UTF-32) [default: %default]")
+    parser.set_defaults(blocksize=16, decimal=False, encoding="UTF-8")
+    opts, files = parser.parse_args()
+    if not (8 <= opts.blocksize <= 80):
+        parser.error("invalid blocksize")
+    if not files:
+        parser.error("no files specified")
 
-    parser.add_option("-b", "--blocksize", type = 'int', 
-                      dest = 'blocksize', default = 16,
-                      help = 'block size(8...80)[default: 16]')
-    parser.add_option("-d", "--decimal", action = "store_false",
-                      help = "decimal block numbers [default: hexadecimal]")
-    parser.add_option("-e", "--encoding", type = "string",
-                      dest = "encoding", default = "utf-8",
-                      help = "encoding (ASCII..UTF-32)[default:UTF-8")
+    for i, filename in enumerate(files):
+        if i:
+            print()
+        if len(files) > 1:
+            print("File:", filename)
+        xdump(filename, opts.blocksize, opts.encoding, opts.decimal)
 
-    (options, args) = parser.parse_args()
-    # print(args)
 
-    print_header()
-    print_binary_data(args, options.blocksize, options.encoding, options.decimal)
+def xdump(filename, blocksize, encoding, decimal):
+    encoding_text = "{0} characters".format(encoding.upper())
+    width = (blocksize * 2) + (blocksize // 4)
+    if blocksize % 4:
+        width += 1
+    print("Block     Bytes{0:{1}} {2}".format("", (width - 5),
+                                              encoding_text))
+    print("--------  {0}  {1}".format("-" * (width - 1),
+          "-" * max(len(encoding_text), blocksize)))
+    block_number_format = "{0:08} " if decimal else "{0:08X} "
+    block_number = 0
+    fh = None
+    try:
+        fh = open(filename, "rb")
+        while True:
+            data = fh.read(blocksize)
+            if not data:
+                break
+            line = [block_number_format.format(block_number)]
+            chars = []
+            for i, b in enumerate(data):
+                if i % 4 == 0:
+                    line.append(" ")
+                line.append("{0:02X}".format(b))
+                chars.append(b if 32 <= b < 127 else ord("."))
+            for i in range(len(data), blocksize):
+                if i % 4 == 0:
+                    line.append(" ")
+                line.append("  ")
+            line.append("  ")
+            line.append(bytes(chars).decode(encoding, "replace")
+                        .replace("\uFFFD", "."))
+            print("".join(line))
+            block_number += 1
+    except EnvironmentError as err:
+        print(err)
+    finally:
+        if fh is not None:
+            fh.close()
 
-def print_header():
-    HEADER = "{block:<8}  {bytes:<35}  {chars:.^10}" \
-                                        .format(block = 'Block',
-                                                bytes = 'Bytes',
-                                                chars = "UTF-8 characters")
-    DIV_LINE = "{0}  {1}  {2}".format('-'*8,
-                                      '-'*35,
-                                      '-'*16)
-    print(HEADER)
-    print(DIV_LINE)
-
-def print_binary_data(files, blocksize = 16, encoding = 'utf-8', decimal = False):
-    if len(files) == 0:
-        return
-    for single_file in files:
-        # print(single_file)
-        brf = BinaryRecordFile(single_file, blocksize - 1)
-        for i in range(len(brf)):
-            result = format_string(brf[i], blocksize, decimal, encoding)
-            # result = None, None
-            print("{0}  {1}  {2}".format(i, result[0], result[1]))
-
-def format_string(binary_record, blocksize, decimal, encoding):
-    s = "<i{}s".format(blocksize - 5)
-    # S = struct.Struct("<i{}s".format(brf[i].record_size - 5))
-    data = struct.unpack(s, binary_record)
-    print(data[1])
-    num_fmt = '0=8d' if decimal == True else "0=8x"
-    num_str = "{0:{fmt}} {1:{fmt}} {2:{fmt}} {3:{fmt}}".format(data[1],
-                                                               int(data[2]),
-                                                               int(data[3]),
-                                                               int(data[4]),
-                                                               fmt = num_fmt)
-    msg_str = "{0:.^10}".format(data[5:].rstrip(b'\x00'))
-    print(num_str)
-    print(msg_str)
-    return num_str, msg_str
 
 main()
